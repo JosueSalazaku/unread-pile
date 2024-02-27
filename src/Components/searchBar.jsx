206eaae810e618fc82d39cb8b7c6c8c75fc8cbb8
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-27 19:05:07 +0100

    Improve input bar and button styling

--- src/Components/searchBar.jsx
+++ src/Components/searchBar.jsx
@@ -5,21 +5,22 @@ const SearchBar = () => {
   const { searchInput, setSearchInput, handleSearch } = useContext(AppSearchContext);
 
   return (
-    <div className="relative">
-      <div className="w-50 inset-y-0 start-0 flex items-center px-20 pointer-events-none">
+    <div className="relative flex h-32 items-center">
       <input
         type="search"
         id="search"
-        className="block w-full rounded-xl h-10 p-4 text-sm text-black border-2 border-black dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="w-80 rounded-l-xl h-10 p-2  text-sm text-black border-l-2 border-t-2 border-b-2 border-black"
         placeholder="Search"
         required
         value={searchInput}
         onChange={(e) => setSearchInput(e.target.value)}
       />
-            <button onClick={handleSearch} className="text-white absolute end-2.5 bottom-0.5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 ml-20 py-2">
-                <img src="src/assets/search-svgrepo-com.svg" alt="" className="h-5" />
-            </button>
-        </div>
+      <button
+        onClick={handleSearch}
+        className="border-r-2 border-t-2 border-b-2 border-black rounded-r-xl  p-2"
+      >
+        <img src="src/assets/search-svgrepo-com.svg" alt="" className="h-5" />
+      </button>
     </div>
   );
 }
--- src/Pages/Browse.jsx
+++ src/Pages/Browse.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import SearchBar from '../Components/SearchBar'
 const Browse = () => {
   return (
-    <main>
+    <main className='flex flex-col items-center'>
       <SearchBar/>
     </main>
   )
