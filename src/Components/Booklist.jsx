59b8ef68828122e337b1b665a532b784bb82853b
Josué Salazaku <josuesalazaku@gmail.com>
2024-02-27 10:31:48 +0100

    Add condition for finding books messaging

--- src/Components/Booklist.jsx
+++ src/Components/Booklist.jsx
@@ -1,7 +1,16 @@
+import { normalizePath } from "vite";
+import { useAppSearch } from "./AppSearchContext";
+
 const Booklist = () => {
-  return (
-    <div>Booklist</div>
-  )
+    const { books, loading, resultTitle } = useAppSearch;
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    if (!books.lenght) {
+        return <p>No books Found.</p>
+    }
 }
 
 export default Booklist
